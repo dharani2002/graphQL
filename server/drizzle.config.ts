3833f22ed76ffd3789b90d7e08f9ccd7a407568d
dharani2002 <rdharani25@gmail.com>
2025-01-31 15:15:10 +0530

    graphQL fullstack project setup

--- server/drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "drizzle-kit";
-
-export default defineConfig({
-  dialect: "mysql",
-  schema:"./drizzle/schema.ts",
-  dbCredentials: {
-    url: "mysql://root:1234@localhost:3306/employees",
-  },
-});
